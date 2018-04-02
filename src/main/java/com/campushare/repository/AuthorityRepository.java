6a2c798678759a28691a48a0f81cc92f4719065b
Alp Çelik <alparslancelik@anadolu.edu.tr>
2018-04-02 07:41:13 +0300

    Initial application generated by JHipster-4.14.1

---
+++ src/main/java/com/campushare/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.campushare.repository;
+
+import com.campushare.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/campushare/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.campushare.service.dto;
